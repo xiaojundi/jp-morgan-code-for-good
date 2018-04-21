ff3652506a5c3b2217a99943720378674e1f409b
Armond St.Juste <astjuste423@gmail.com>
2018-04-21 16:15:46 -0400

    Finished Matching logic

--- server/index.js
+++ server/index.js
@@ -12,6 +12,35 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(compression());
 
+const TEST_VOLUNTEERS = [{
+    username: 'Jimmy Dean',
+    role: 'volunteer',
+    firstName: 'Jimmy',
+    lastName: 'Dean',
+    age: 24,
+    interests: [{ name: 'Soccer', description: 'I like to kick stuff' }, { name: 'baseball', description: 'I like to hit stuff' }, { name: 'video games', description: 'I will beat anyone in Call of Duty' }, { name: 'pokemon', description: 'Pretty self explanitory' }],
+    gender: 'Male',
+    preferredCommunication: 'In Person',
+    email: 'JimmyD@example.com',
+    employed: false,
+    bestTimeToSocialize: 'Weekends',
+    travelIndependent: true,
+    zipCode: 11238,
+    borough: 'Brooklyn',
+    description: 'Lorum Ipsum'
+},{
+    username: 'Tanto12',
+    role:'volunteer',
+    firstName: 'Terell',
+    lastName: 'Brown',
+    age:23,
+    interests:[{name:'Basketball',description:'ball is life'},{name:'Sports',description:"I'm competetive"},{name:'Reading',description:'Books'},{name:'Exercise',description:'Pick things up'},{name:'Soccer',description:'Messi'}],
+    bestTimeToSocialize:'Weekends',
+    preferredCommunication: 'Text',
+    borough: 'Brooklyn',
+    gender: 'Male',
+    zipCode: 11001
+}]
 //app.use(require('./routes'));//
 
 app.get('/buddyInfo', (req, res, next) => {
@@ -66,9 +95,56 @@ app.get('/alreadyPaired', (req, res, next) => {
 })
 
 app.get('/potentialPairs', (req, res, next) => {
-    return res.status(200).send({
+    const buddy = {
+        username: 'Billy D',
+        role: 'buddy',
+        firstName: 'Billy',
+        lastName: 'Doe',
+        age: 20,
+        interests: [{ name:'Soccer', description:'I like to kick stuff'}, { name:'baseball', description:'I like to hit stuff'}, { name:'video games', description:'I will beat anyone in Call of Duty'}, { name:'pokemon', description:'Pretty self explanitory'}],
+        gender: 'Male',
+        preferredCommunication: 'In Person',
+        email: 'BillyD@example.com',
+        employed: false,
+        bestTimeToSocialize: 'Weekends',
+        travelIndependent: true,
+        zipCode: 11238,
+        borough: 'Brooklyn',
+        description: 'Lorem Ipsum'
+    }
+    const matches = {}
 
+    TEST_VOLUNTEERS.forEach((volunteer)=>{
+        let score = 0;
+        if ((volunteer.gender !== 'Female') && (volunteer.gender !== buddy.gender)) {
+            return false;
+        }
+        //In actuality this would be a range comparison
+        if(volunteer.borough != buddy.borough){
+            return false;
+        }
+
+        buddy.interests.forEach((interest)=>{
+            //console.log(interest);
+           // console.log(volunteer.interests.includes(interest));
+            volunteer.interests.forEach((vInterest)=>{
+                if(interest.name == vInterest.name){
+                    score++;
+                }
+            })
+        });
+
+        if(buddy.preferredCommunication !== volunteer.preferredCommunication){
+            score = score * 0.5;
+        }
+        if(buddy.bestTimeToSocialize !== volunteer.bestTimeToSocialize){
+            score = score * 0.9
+        }
+        let fullname = volunteer.firstName + volunteer.lastName;
+
+        matches[fullname] = score;
     })
+    res.status(200).send(matches);
 });
 
 app.get('/volunteerWaitlist', (req, res, next) => {
